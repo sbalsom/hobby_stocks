cede346d603e7d67dcb50b19fe537a697b1bcbdb
Sadie Balsom <saralogsin@protonmail.com>
2020-09-20 21:31:05 +0200

    refactors and cleans up FE code

--- README.md
+++ README.md
@@ -4,19 +4,32 @@ A hobby app that intends to fetch (Apple) stock prices from Tiingo.com api and s
 
 ENV variables to add to `.env` file:
 
-'PGPORT' is a configurable ENV variable in order to use the project database on a port other than the defualt PostGres port
+'PGPORT' is a configurable ENV variable in order to use the project database on a port other than the default PostGres port
+
 'TIINGO_TOKEN': In order for the API calls to work, you must have a working Tiingo API token, and it must be saved as an ENV variable
 
+Token can be created here for free : https://api.tiingo.com/
+
 some known issues come up with reading these env variables (for unknown to me reasons), so you may need to run `TIINGO_TOKEN=<YOUR TOKEN> iex -S mix` for it to actually work
+
 To start Phoenix server:
 
   * Install dependencies with `mix deps.get`
   * Create and migrate database with `mix ecto.setup`
   * Start Phoenix endpoint with `mix phx.server`
 
-Now you can visit [`localhost:4000`](http://localhost:4000) from your browser.
-
 As soon as the server is started with either `mix phx.server` or `iex -S mix`, the stock data will begin fetching and saving. Requests run once an hour.
+Starting the server also boots up the websockets client, which streams data from Coinbase for 4 different cryptocurrency products ("BTC-USD, "BTC-EUR", "ETH-USD', "ETH-EUR"). Navigating to the show page of each of these stocks will take you to a livestream of that stocks prices.
 
-Only two endpoints exist on the user interface : An index and a show page. Neither of them are currently used, but in the future could be used to display available stocks on Tiingo API, and possibly allow the user to select a specific stock they would like to have saved, and save it to a CSV file for data analysis.
+The endpoints are as follows :
+Index all stocks (WIP) :
+`http://localhost:4000/stocks`
 
+Show for one stock (static - WIP):
+`http://localhost:4000/stocks/<SYMBOL>`
+
+Stream for one stock:
+`http://localhost:4000/livestocks/stream?ticker=<SYMBOL>&channel=<CHANNEL_TYPE>`
+where SYMBOL = one of the cryptocurrency product symbols, and CHANNEL_TYPE can be one of "ticker" or "match"
+
+![Screenshot 2020-09-20 at 21 34 08](https://user-images.githubusercontent.com/46577238/93720553-171c2300-fb8a-11ea-9942-859d6363dbb3.png)
--- assets/js/ticker.js
+++ assets/js/ticker.js
@@ -1,131 +1,138 @@
 import Chart from 'chart.js';
 
+let price = document.getElementById("price");
+let time = document.getElementById("time")
+let size = document.getElementById("size")
+let type = document.getElementById("type")
+
+function writeToPage(p, d, sz, sd) {
+  price.innerHTML = `${p}`
+  time.innerHTML = `${d.toLocaleString()}`
+  size.innerHTML = `${sz}`
+  type.innerHTML = `${sd}`
+}
+
+function writeToChart(price, time) {
+  let newData = {
+    t: time, y: price
+  }
+  let dataset = myChart.data.datasets[0].data
+  dataset.push(newData)
+  if (dataset.length > 200) {
+    dataset.shift();
+  }
+  myChart.update();
+
+}
+
+function extractData(set) {
+  return { t: new Date(set["time"]), y: parseFloat(set["price"]) }
+}
+
+function writeAllToChart(cache) {
+  console.log("writing all ")
+  console.log(cache.length)
+  myChart.data.datasets[0].data = cache.map(extractData).sort(function (a, b) {
+    return a.t - b.t;
+  })
+  myChart.update();
+}
+
+let ctx = document.getElementById('myChart').getContext('2d');
+let myChart = new Chart(ctx, {
+  type: 'scatter',
+  data: {
+
+    datasets: [{
+      label: 'Stock Price (last 200 sales)',
+      showLine: true,
+      data: [],
+      borderWidth: 1,
+      color: 'white',
+      borderColor: 'rgba(189, 23, 59, 1)',
+      backgroundColor: 'rgba(176, 245, 102, 0.3)',
+      pointBorderColor: 'rgba(222, 214, 236, 1.00)',
+      capBezierPoints: false,
+      tension: 0,
+      stepped: true,
+      hitRadius: 0.5,
+      fill: 'origin'
+    }]
+  },
+  options: {
+    legend: {
+      labels: {
+        fontColor: '#FFFFFF'
+      }
+    },
+    scales: {
+      xAxes: [{
+        type: 'time',
+        position: 'bottom'
+      }],
+      yAxes: [{
+        ticks: {
+          precision: 0.001,
+          fontColor: '#FFFFFF'
+        }
+      }]
+    }
+  }
+});
+
 let Ticker = {
   init(socket) {
 
     const urlParams = new URLSearchParams(window.location.search);
     let ticker = urlParams.get('ticker');
     let channelType = urlParams.get('channel');
+
     if (ticker && channelType) {
-
-      var ctx = document.getElementById('myChart').getContext('2d');
-      var myChart = new Chart(ctx, {
-        type: 'scatter',
-        data: {
-
-          datasets: [{
-            label: 'Stock Price (last 30 sales)',
-            showLine: true,
-            data: [],
-            borderWidth: 1,
-            color: 'white',
-            borderColor: 'rgba(189, 23, 59, 1)',
-            backgroundColor: 'rgba(176, 245, 102, 0.3)',
-            pointBorderColor: 'rgba(222, 214, 236, 1.00)',
-            capBezierPoints: false,
-            tension: 0,
-            stepped: true,
-            hitRadius: 0.5,
-            fill: 'origin'
-          }]
-        },
-        options: {
-          legend: {
-            labels: {
-              fontColor: '#FFFFFF'
-            }
-          },
-          scales: {
-            xAxes: [{
-              type: 'time',
-              position: 'bottom'
-            }],
-            yAxes: [{
-              ticks: {
-                precision: 0.001,
-                fontColor: '#FFFFFF'
-              }
-            }]
-          }
-        }
-      });
-
-
       if (channelType == "ticker") {
-        let channel = socket.channel(`ticker:${ticker}`, {})
-        let dataset = myChart.data.datasets[0].data
 
-        let price = document.getElementById("price");
-        let time = document.getElementById("time")
-        let size = document.getElementById("size")
-        let type = document.getElementById("type")
+        let channel = socket.channel(`ticker:${ticker}`, {})
 
         channel.join()
           .receive("ok", resp => { console.log("Joined successfully", resp) })
           .receive("error", resp => { console.log("Unable to join", resp) });
 
+        channel.on("cache_load", payload => {
+          writeAllToChart(payload["cache"]);
+        });
         channel.on("ticker_event", payload => {
-          console.log(payload)
-
           let newPrice = parseFloat(payload["price"])
           let newDate = new Date(payload["time"])
-          let side = payload["side"]
+          let newSide = payload["side"]
+          let newSize = payload["last_size"]
 
-          let newData = {
-            t: newDate, y: newPrice
-          }
-          dataset.push(newData)
-          console.log(dataset.length)
-          // if (dataset.length > 100000) {
-          //   dataset.shift();
-          // }
-          myChart.update();
-          console.log(dataset)
-          price.innerHTML = `${newPrice}`
-          size.innerHTML = `${payload["last_size"]}`
-          time.innerHTML = `${newDate.toLocaleString()}`
-          type.innerHTML = `${side}`
+          writeToPage(newPrice, newDate, newSize, newSide);
+          writeToChart(newPrice, newDate)
+
         });
+
       } else if (channelType == "match") {
         let channel = socket.channel(`match:${ticker}`, {})
 
-        let price = document.getElementById("price");
-        let time = document.getElementById("time")
-        let size = document.getElementById("size")
-        let type = document.getElementById("type")
-
         channel.join()
           .receive("ok", resp => { console.log("Joined successfully", resp) })
           .receive("error", resp => { console.log("Unable to join", resp) });
 
-
-
         channel.on("match_event", payload => {
-          console.log(payload)
-          let dataset = myChart.data.datasets[0].data
-
           let newPrice = parseFloat(payload["price"])
           let newDate = new Date(payload["time"])
-          let side = payload["side"]
-          let newData = {
-            t: newDate, y: newPrice
-          }
-          dataset.push(newData)
+          let newSide = payload["side"]
+          let newSize = payload["size"]
 
-          if (dataset.length > 30) {
-            dataset.shift();
-          }
-          myChart.update();
-          console.log(dataset)
-          price.innerHTML = `${newPrice}`
-          size.innerHTML = `${payload["size"]}`
-          time.innerHTML = `${newDate.toLocaleString()}`
-          type.innerHTML = `${side}`
+          writeToPage(newPrice, newDate, newSize, newSide)
+          writeToChart(newDate, newPrice);
+
         });
       }
 
     }
   }
+
+
 }
 
 export default Ticker;
