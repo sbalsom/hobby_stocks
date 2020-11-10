f98de0487fe883552d4ef64e848831d9a2d7ef70
Sadie Balsom <saralogsin@protonmail.com>
2020-11-10 22:38:17 +0100

    fakes user token

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -4,7 +4,7 @@
 import { Socket } from "phoenix"
 
 
-let socket = new Socket("/stream")
+let socket = new Socket("/stream", { params: { token: "SFMyNTY.g2gDYQFuBgB28xe0dQFiAAFRgA.yELUI2UFcPc8LUX4Pfai3ZPcSJfZXYsVGKtelXoSAMA" } })
 
 socket.connect()
 
